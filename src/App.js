b38a68c17bb00ca7892ba0588b42aab00e241d06
Brian Crowe <brian.j.crowe@protonmail.com>
2025-09-17 19:35:19 -0400

    Header, Teachers, Courses rendered with Routes

--- src/App.js
+++ src/App.js
@@ -4,13 +4,19 @@ import { Route, Routes } from 'react-router-dom'
 // App Components
 import Home from './components/Home.js'
 import About from './components/About.js'
+import Header from './components/Header.js'
+import Teachers from './components/Teachers.js'
+import Courses from './components/Courses.js'
 
 const App = () => {
   return (
     <div className="container">
+      <Header />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="about" element={<About />} />
+        <Route path="teachers" element={<Teachers />} />
+        <Route path="courses" element={<Courses />} />
       </Routes>
     </div>
   )
