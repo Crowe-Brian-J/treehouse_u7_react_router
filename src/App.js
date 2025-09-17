5689fd2337598fae3463713bce2ade2602201df7
Brian Crowe <brian.j.crowe@protonmail.com>
2025-09-17 19:30:27 -0400

    Routes, Route added for Home and About

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
-import React from "react";
+import React from 'react'
+import { Route, Routes } from 'react-router-dom'
 
-function App() {
+// App Components
+import Home from './components/Home.js'
+import About from './components/About.js'
+
+const App = () => {
   return (
     <div className="container">
-
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="about" element={<About />} />
+      </Routes>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
