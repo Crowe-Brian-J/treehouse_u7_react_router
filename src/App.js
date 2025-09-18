2a50d40669ad5e07ab2858169c9bbe0a9cedeca6
Brian Crowe <brian.j.crowe@protonmail.com>
2025-09-17 20:58:32 -0400

    move descendant routes to App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Route, Routes } from 'react-router-dom'
+import { Route, Routes, Navigate } from 'react-router-dom'
 
 // App Components
 import Home from './components/Home.js'
@@ -7,6 +7,9 @@ import About from './components/About.js'
 import Header from './components/Header.js'
 import Teachers from './components/Teachers.js'
 import Courses from './components/Courses.js'
+import HTML from './components/courses/HTML'
+import CSS from './components/courses/CSS'
+import JavaScript from './components/courses/JavaScript'
 
 const App = () => {
   return (
@@ -16,7 +19,12 @@ const App = () => {
         <Route path="/" element={<Home />} />
         <Route path="about" element={<About />} />
         <Route path="teachers" element={<Teachers />} />
-        <Route path="courses" element={<Courses />} />
+        <Route path="courses" element={<Courses />}>
+          <Route index element={<Navigate replace={true} to="html" />} />
+          <Route path="html" element={<HTML />} />
+          <Route path="css" element={<CSS />} />
+          <Route path="javascript" element={<JavaScript />} />
+        </Route>
       </Routes>
     </div>
   )
